901c378644f43e982c22c699f37ef82790412cb4
maha <maha@gmail.com>
2020-06-04 07:40:12 +0530

    Maven Project & TestNG Classs Creation

---
+++ MavenSeleniumBuild_6_30AMBatch/src/main/java/com/project/MavenSeleniumBuild_6_30AMBatch/App.java
@@ -0,0 +1,13 @@
+package com.project.MavenSeleniumBuild_6_30AMBatch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
