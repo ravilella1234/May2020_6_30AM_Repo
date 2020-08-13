43da48a6ed51fcf76de4313360e4679ad6c825a9
maha <maha@gmail.com>
2020-08-13 06:59:18 +0530

    Hybrid day-8

--- CRM/src/main/java/com/project/CRM/dirverClass/DriverScript.java
+++ CRM/src/main/java/com/project/CRM/dirverClass/DriverScript.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 import java.util.Properties;
 
 import com.project.CRM.keywordClasses.AppKeywords;
+import com.project.CRM.utilities.Constants;
 import com.project.CRM.utilities.ExcelAPI;
 
 public class DriverScript 
@@ -23,7 +24,7 @@ public class DriverScript
 	public void executeKeywords(ExcelAPI xls,String testName,Hashtable<String, String> testData) throws Exception, SecurityException
 	{
 		System.out.println("iam driverscript class....");
-		int rows = xls.getRowCount("Keyword");
+		int rows = xls.getRowCount(Constants.KEYWORD_SHEET);
 		System.out.println("Rows : " + rows);
 		app = new AppKeywords();
 		app.setOrProp(orProp);
@@ -32,13 +33,13 @@ public class DriverScript
 		
 		for(int rNum=1;rNum<rows;rNum++)
 		{
-			String tcid = xls.getCellData("Keyword", "TCID", rNum);
+			String tcid = xls.getCellData(Constants.KEYWORD_SHEET, Constants.TCID_COL, rNum);
 			
 			if(tcid.equals(testName))
 			{
-				String kWord = xls.getCellData("Keyword", "Keywords", rNum);
-				String objectKey = xls.getCellData("Keyword", "Object", rNum);
-				String dataKey = xls.getCellData("Keyword", "Data", rNum);
+				String kWord = xls.getCellData(Constants.KEYWORD_SHEET, Constants.KEYWORD_COL, rNum);
+				String objectKey = xls.getCellData(Constants.KEYWORD_SHEET, Constants.OBJECT_COL, rNum);
+				String dataKey = xls.getCellData(Constants.KEYWORD_SHEET, Constants.DATA_COL, rNum);
 				String data = testData.get(dataKey);
 				
 				//System.out.println(tcid +"----" + kWord + "----" + orProp.getProperty(objectKey) +"----" + data);
